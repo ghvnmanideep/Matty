b4337dc8bfc6aa7c5ca9910ce4ba51785941f10b
Gattupalli Hema Venkata Naga Mani Deep <ghvnmanideep@gmail.com>
2025-08-19 20:09:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,17 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
+// CORS configuration
 app.use(
   cors({
-    origin: "https://matty-p9zo.vercel.app/signin", // Adjust this if your frontend is hosted elsewhere
+    origin: "https://matty-p9zo.vercel.app", // Adjust this if your frontend is hosted elsewhere
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
+    credentials: true,
+  })
 );
+
+// Middleware for parsing request bodies
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
